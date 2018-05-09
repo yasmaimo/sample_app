3e912fef1b38907bbc0646fcc3bbe986a11c4f68
yasmaimo <yasmaimo0141@gmail.com>
2018-05-09 14:56:30 +0900

    Finish 3-9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,13 @@ class PostsController < ApplicationController
   	  redirect_to post_path(post.id)
   end
 
+  def destroy
+  	  post = Post.find(params[:id])
+  	  post.destroy
+  	  redirect_to posts_path
+  end
+
+
   private
 
   def post_params
