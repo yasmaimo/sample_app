af1f7aad088b0633253ad789e7f329cf15ad4772
yasmaimo <yasmaimo0141@gmail.com>
2018-05-09 14:34:32 +0900

    Finish 3-8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-  	  params.require(:post).permit(:title, :body)
+  	  params.require(:post).permit(:title, :body, :image)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509020151) do
+ActiveRecord::Schema.define(version: 20180509051826) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
